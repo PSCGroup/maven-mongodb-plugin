83f5b5deb431b9bccd5d450a25a63b7330ff3b83
Mike De Haan <mdehaan@psclistens.com>
2015-09-23 12:30:43 -0500

    Added Training target and updated release process

---
+++ src/main/java/com/nesting/maven2/mongodb/MongoDBTrainingMojo.java
@@ -0,0 +1,45 @@
+package com.nesting.maven2.mongodb;
+
+import com.mongodb.DB;
+import com.mongodb.Mongo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * Mojo for filling databases with data.
+ * @goal update
+ */
+public class MongoDBTrainingMojo
+    extends AbstractMongoDBMojo {
+
+    /**
+     * The directory that contains update scripts.
+     * @parameter
+     * @required
+     */
+    private File[] dbTrainingScriptsDirectory;
+
+    /**
+     * {@inheritDoc}
+     */
+    public void executeInternal()
+        throws MojoExecutionException,
+        MojoFailureException {
+
+        try {
+            Mongo mongo = openConnection();
+            DB db = getDatabase(mongo);
+            for (int i=0; i<dbTrainingScriptsDirectory.length; i++) {
+                executeScriptsInDirectory(
+                        dbTrainingScriptsDirectory[i], db);
+            }
+
+        } catch(IOException ioe) {
+            throw new MojoExecutionException(
+                "Error executing training scripts", ioe);
+        }
+    }
+}
